1edf15b01beccd9d9f02d96326f5e59f1327e0ba
ersantoli <74686375+ersantoli@users.noreply.github.com>
2022-09-12 15:32:50 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import Home from './src/screens/home';
+import { StatusBar } from 'react-native';
+
+
+export default function App(){
+  return(
+    <>
+       <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        />
+       <Home />
+    </>
+ 
+  );
+}
+
